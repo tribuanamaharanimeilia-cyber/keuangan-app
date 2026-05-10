27c8ddb3761c8f328bd5f8d92816e04ab00a9f42
Meilia Tribuana Maharani <tribuanamaharanimeilia@gmail.com>
2026-05-10 20:59:42 +0700

    fix api database

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { Pool } = require("pg");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
@@ -20,55 +21,92 @@ pool.connect()
   .then(() => console.log("DB CONNECTED"))
   .catch(err => console.error("DB ERROR:", err));
 
+
+// GET DATA
 app.get("/data", async (req, res) => {
-  const result = await pool.query("SELECT * FROM keuangan ORDER BY id DESC");
-  res.json(result.rows);
+  try {
+
+    const result = await pool.query(
+      "SELECT * FROM keuangan ORDER BY id DESC"
+    );
+
+    res.json(result.rows);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error ambil data");
+  }
 });
 
+
+// TAMBAH DATA
 app.post("/data", async (req, res) => {
+
   try {
+
     const { deskripsi, jumlah, tipe } = req.body;
 
     await pool.query(
-      "INSERT INTO keuangan (deskripsi, jumlah, tipe) VALUES ($1, $2, $3)",
+      "INSERT INTO keuangan (deskripsi, jumlah, tipe) VALUES ($1,$2,$3)",
       [deskripsi, jumlah, tipe]
     );
 
-    res.send("OK");
+    res.send("Berhasil tambah");
+
   } catch (err) {
     console.error(err);
     res.status(500).send("Database Error");
   }
 });
 
-app.listen(3000, () => console.log("Server jalan di 3000"));
 
+// EDIT DATA
 app.put("/data/:id", async (req, res) => {
-  const { deskripsi, jumlah, tipe } = req.body;
-  const id = req.params.id;
 
-  await pool.query(
-    "UPDATE keuangan SET deskripsi=$1, jumlah=$2, tipe=$3 WHERE id=$4",
-    [deskripsi, jumlah, tipe, id]
-  );
+  try {
 
-  res.send("Updated");
+    const { deskripsi, jumlah, tipe } = req.body;
+
+    const id = req.params.id;
+
+    await pool.query(
+      "UPDATE keuangan SET deskripsi=$1, jumlah=$2, tipe=$3 WHERE id=$4",
+      [deskripsi, jumlah, tipe, id]
+    );
+
+    res.send("Updated");
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Update Error");
+  }
 });
 
+
+// HAPUS DATA
 app.delete("/data/:id", async (req, res) => {
-  const id = req.params.id;
-  await pool.query("DELETE FROM keuangan WHERE id=$1", [id]);
-  res.send("Deleted");
+
+  try {
+
+    const id = req.params.id;
+
+    await pool.query(
+      "DELETE FROM keuangan WHERE id=$1",
+      [id]
+    );
+
+    res.send("Deleted");
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Delete Error");
+  }
 });
 
-app.put("/data/:id", async (req, res) => {
-  const { deskripsi, jumlah, tipe } = req.body;
-  const id = req.params.id;
 
-  await pool.query(
-    "UPDATE keuangan SET deskripsi=$1, jumlah=$2, tipe=$3 WHERE id=$4",
-    [deskripsi, jumlah, tipe, id]
-  );
+// SERVER
+const PORT = process.env.PORT || 3000;
 
-  res.send("Updated");
+app.listen(PORT, () => {
+  console.log(`Server jalan di ${PORT}`);
 });
